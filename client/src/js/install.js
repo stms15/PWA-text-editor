5e9ffa420a35a7b5263770882c06130b451045b3
Sarah S <stms15@mun.ca>
2023-09-15 08:54:14 -0230

    added install handlers

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,18 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  butInstall.style.visibility = 'visible';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  installBtn.setAttribute('disabled', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+  installBtn.textContent = 'Installed!';
+  console.log('👍', 'appinstalled', event);
+});

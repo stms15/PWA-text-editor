52de8b8978740b3aca0888cc441ce0b890bc1a7a
Sarah S <stms15@mun.ca>
2023-09-15 09:30:23 -0230

    configured webpack js file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,74 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+// Configure workbox plugins for service worker and manifest files.
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE Text Editor',
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'JATE Text Editor',
+        short_name: 'JATE',
+        description: 'Text editor that runs in the browser and works offline.',
+        background_color: '#272822',
+        theme_color: '#31a9e1',
+        start_url: './',
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            type: 'image/png',
+            sizes: [96, 128, 192, 512],
+            purpose: 'any maskable',
+          },
+        ],
+      }),
     ],
 
+    // Add CSS loaders and babel
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime',
+              ],
+            },
+          },
+        },
       ],
     },
   };
